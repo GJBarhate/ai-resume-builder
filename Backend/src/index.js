236bf203aa9565187b001c9e55fe41ac5c15770a
GJBarhate <gauravbarhate55@gmail.com>
2025-09-01 02:30:15 +0530

    index.js solve

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,10 +1,13 @@
 import app from "./app.js";
 import { connectDB } from "./db/index.js";
 import { config } from "dotenv";
+
 config();
 
+const PORT = process.env.PORT || 5001; // fallback for local dev
+
 connectDB().then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log("Server is running on http://localhost:" + process.env.PORT);
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
 });
