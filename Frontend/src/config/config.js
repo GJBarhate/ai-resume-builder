2bcd1e1b6f4643915c48ce69be33c28671c73c22
GJBarhate <gauravbarhate55@gmail.com>
2025-09-22 02:13:50 +0530

    Update config.js

--- Frontend/src/config/config.js
+++ Frontend/src/config/config.js
@@ -1,6 +1,6 @@
 const AUTH_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 const API_KEY = import.meta.env.VITE_STRAPI_API_KEY;
 const GEMENI_API_KEY = import.meta.env.VITE_GEMENI_API_KEY;
-const VITE_APP_URL = import.meta.env.VITE_APP_URL;
+const VITE_APP_URL = "https://ai-resume-builder-backendone.onrender.com/";
 
 export { AUTH_KEY, API_KEY, GEMENI_API_KEY,VITE_APP_URL };
